bc144749cbdd5cd84ea3103552d2bee58bbe8447
dilip640 <2017csb1073@iitrpr.ac.in>
2018-08-31 17:57:39 +0530

    as

--- zmain/js/main.js
+++ zmain/js/main.js
@@ -72,25 +72,6 @@
 	});
 
 
-	/*----------------------------------------------------*/
-  	/* Smooth Scrolling
-  	------------------------------------------------------*/
-  	$('.smoothscroll').on('click', function (e) {
-	 	
-	 	e.preventDefault();
-
-   	var target = this.hash,
-    	$target = $(target);
-
-    	$('html, body').stop().animate({
-       	'scrollTop': $target.offset().top
-      }, 800, 'swing', function () {
-      	window.location.hash = target;
-      });
-
-  	});
-
-
   	/* --------------------------------------------------- */
 	/*  Placeholder Plugin Settings
 	------------------------------------------------------ */
